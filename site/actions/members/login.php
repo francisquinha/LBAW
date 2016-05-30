c7db851636d1edeaf8f7b7f3acd15628c275115a
francisquinha <francisquinha@gmail.com>
2016-05-30 16:28:40 +0100

    get login info

--- site/actions/members/login.php
+++ site/actions/members/login.php
@@ -1,24 +1,27 @@
 <?php
-  include_once('../../config/init.php');
-  include_once($BASE_DIR .'database/members.php');
+include_once('../../config/init.php');
+include_once($BASE_DIR . 'database/members.php');
 
-  if (!$_POST['username'] || !$_POST['password']) {
+if (!$_POST['username'] || !$_POST['password']) {
     $_SESSION['error_messages'][] = 'Invalid login';
     $_SESSION['form_values'] = $_POST;
     header('Location: ' . $_SERVER['HTTP_REFERER']);
     exit;
-  }
+}
 
-  $username = $_POST['username'];
-  $password = $_POST['password'];
+$username = $_POST['username'];
+$password = $_POST['password'];
+$userinfo = getLogin($username, $password);
 
-  if (isLoginCorrect($username, $password)) {
+if ($userinfo) {
     $_SESSION['username'] = $username;
+    $_SESSION['permissiontype'] = $userinfo['permissiontype'];
+    $_SESSION['userid'] = $userinfo['memberid'];
     $_SESSION['success_messages'][] = 'Login successful';
     echo 'true';
-  } else {
+} else {
     $_SESSION['error_messages'][] = 'Login failed';
     echo 'false';
 }
-//header('Location: ' . $_SERVER['HTTP_REFERER']);
+//header('Location: ' . $_SERVER['HTTP_REFERER']);*/
 ?>
