9882c7b07d69a2a53c053955ce0201c0c365de86
Nuno Valente <nunomrvalente@gmail.com>
2016-06-02 14:43:48 +0100

    rm templates_c, init.php, main.js

--- site/javascript/main.js
+++
@@ -1,7 +0,0 @@
-//BASE_URL = "https://gnomo.fe.up.pt/~lbaw1553";
-BASE_URL = "http://localhost/LBAW/site";
-//BASE_URL = "https://gnomo.fe.up.pt/~ei12168"
-//BASE_URL = "https://gnomo.fe.up.pt/~up200204376"
-//BASE_URL = "https://gnomo.fe.up.pt/~ei01020"
-
-
