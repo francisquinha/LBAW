61e8e91538325cf651b882bb68394e78666c6c04
Ângela Cardoso <francisquinha@gmail.com>
2016-06-03 13:44:22 +0100

    Update details.php
    assign not needed

--- site/pages/questions/details.php
+++ site/pages/questions/details.php
@@ -42,7 +42,6 @@ if (isset($_GET['questionid'])) {
         $answers[$key]['versionbody'] = $answer_body['versionbody'];
     }
 
-    $smarty->assign('question_id', $_GET['questionid']);
     $smarty->assign('bestanswer', $bestanswer);
     $smarty->assign('answers', $answers);
     $smarty->assign('question', $question);
